373139e4c33a46d294456611a251fc178b431915
yoonyoungheon <tbvjgjsl9@naver.com>
2023-04-03 19:13:38 +0900

    start

--- #main.py
+++ #main.py
@@ -1,6 +1,6 @@
 #main
-name = input("이름을 입력하세요: ")
-id = int(input("학번을 입력하세요: "))
-major = input("학과를 입력하세요: ")
+name = input("이름을 입력하세요.: ")
+id = int(input("학번을 입력하세요.: "))
+major = input("학과를 입력하세요.: ")
 
 print(f"이름: {name} \n학번: {id} \n학과: {major}")
