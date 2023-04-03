43b4890b3dc1d7b2221b7f0a25b728e73d1315da
yoonyoungheon <tbvjgjsl9@naver.com>
2023-04-03 16:04:06 +0900

    add univ info

--- #main.py
+++ #main.py
@@ -2,4 +2,5 @@
 name = input("이름을 입력하세요: ")
 id = int(input("학번을 입력하세요: "))
 major = input("학과를 입력하세요: ")
-print(f"이름: {name} \n학번: {id} \n학과: {major}")
+univ = input("학교를 입력하세요: ")
+print(f"이름: {name} \n학번: {id} \n학과: {major} \n학교: {univ}")
